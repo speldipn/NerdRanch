dd552d6ea8e57209a9d632232da338209368bfdf
Neo <speldipn@gmail.com>
2021-04-20 07:22:09 +0900

    checking answer and next, show score

--- app/src/main/java/com/example/nerdranch/MainActivity.kt
+++ app/src/main/java/com/example/nerdranch/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.nerdranch
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var rootView: LinearLayout
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var previousButton: ImageButton
@@ -25,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             Question(R.string.question_americas, true),
             Question(R.string.question_asia, true),
     )
+    private var answerCount = 0
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,15 +35,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setup() {
+        rootView = findViewById(R.id.rootView)
         questionTextView = findViewById(R.id.questionTextView)
         trueButton = findViewById(R.id.trueButton)
         falseButton = findViewById(R.id.falseButton)
         previousButton = findViewById(R.id.previousButton)
         nextButton = findViewById(R.id.nextButton)
 
-        trueButton.setOnClickListener { checkAnswer(true) }
+        trueButton.setOnClickListener {
+            checkAnswer(true)
+            nextButton.callOnClick()
 
-        falseButton.setOnClickListener { checkAnswer(false) }
+        }
+
+        falseButton.setOnClickListener {
+            checkAnswer(false)
+            nextButton.callOnClick()
+        }
 
         previousButton.setOnClickListener {
             currentIndex = if (currentIndex == 0) {
@@ -56,10 +64,16 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questions.size
+            if(currentIndex == 0) { showScore() }
             updateQuestion()
         }
 
         updateQuestion()
+    }
+
+    private fun showScore() {
+        Snackbar.make(rootView, "${answerCount}개 정답", Snackbar.LENGTH_SHORT).show()
+        answerCount = 0
     }
 
     private fun updateQuestion() {
@@ -67,16 +81,19 @@ class MainActivity : AppCompatActivity() {
         questionTextView.setText(questionTextResId)
     }
 
-    private fun checkAnswer(userAnswer: Boolean) {
+    private fun checkAnswer(userAnswer: Boolean): Boolean {
         val correctAnswer = questions[currentIndex].answer
 
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.answer_pass
+            answerCount += 1
         } else {
             R.string.answer_not_pass
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        return messageResId == R.string.answer_pass
     }
 
     private fun print(msg: String) = Log.d(TAG, msg)
